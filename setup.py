909cf38c27ee37e70fa50c8fefa105fb9363f65e
Brian Darrow <brian@digitalrunning.com>
2017-01-22 18:49:39 -0500

    added license

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='atbat',
+    version='0.1',
+    description='module that exposes the log5 and Morey-Z methods for calculating the probability that a given event will happen during a baseball plate appearance given the batter, pitcher and league average probabilities of that event happening.',
+    url='',
+    author='Brevetoxin',
+    author_email='BrianPDarrow@gmail.com',
+    license='MIT',
+    packages=['atbat'])
